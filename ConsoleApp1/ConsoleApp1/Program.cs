0880c31f53a71e829e8df9e986ef1405bf9972fc
Predator <murad.mammedzade11@gmail.com>
2023-08-27 06:58:59 -0700

    save

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello World";
+            string input = word.ToLower();
+            int vowelsCount = 0;
+            int consonatsCount = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'o' || input[i] == 'u' || input[i] == 'i' || input[i] == 'e' || input[i] == 'ı' || input[i] == 'ə' || input[i] == 'ö' || input[i] == 'ü')
+                {
+                    vowelsCount++;
+                }
+                else if (input[i] != ' ')
+                {
+                    consonatsCount++;
+                }
+            }
+            Console.WriteLine("Vowels : " + vowelsCount);
+            Console.WriteLine("Consonats : " + consonatsCount);
+        }
+    }
+}
